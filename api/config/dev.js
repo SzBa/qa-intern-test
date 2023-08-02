818ab984837388460a26281ba08417e23408bb3c
SzBa <141048582+SzBa@users.noreply.github.com>
2023-08-02 16:23:47 +0200

    Fixed tests and add more cases
    Fixed base URL / add more cases

--- api/config/dev.js
+++ api/config/dev.js
@@ -1,3 +1,3 @@
 export const config = {
-  baseUrl: " https://api.tvmaze.com",
+  baseUrl: "https://api.tvmaze.com",
 };
--- api/test/show-search/GET_show-search.test.js
+++ api/test/show-search/GET_show-search.test.js
@@ -1,18 +1,40 @@
 import request from "supertest";
 import { config } from "../../config/dev.js";
 import {
+  error,
   getShowSearchRequiredProperties,
   showSearchUrl,
 } from "../../utilities/show-search/show-search.js";
 import { shouldHaveProperties } from "../../utilities/helpers.js";
+import should from "should";
 
 describe("GET /search/shows", () => {
-  it("should get all the shows by the shows name girls", async () => {
+  it("should get shows by the shows girls name", async () => {
     await request(config.baseUrl)
       .get(showSearchUrl())
       .expect(({ body }) => {
-        shouldHaveProperties(body, getShowSearchRequiredProperties);
+        shouldHaveProperties(body[0], getShowSearchRequiredProperties);
       })
       .expect(200);
+  });
+
+  it("should not get shows by the shows name when q is undefined", async () => {
+    await request(config.baseUrl)
+      .get(showSearchUrl({ q: undefined }))
+      .expect(({ body }) => {
+        should.equal(body.name, error.name);
+        should.equal(body.message, error.message);
+      })
+      .expect(400);
+  });
+
+  // Bug reported to trello
+  it("should not get shows by the shows name when q is incorrect value", async () => {
+    await request(config.baseUrl)
+      .get(showSearchUrl({ q: "cosfgfd" }))
+      .expect(({ body }) => {
+        should.equal(body.name, error.name);
+      })
+      .expect(400);
   });
 });
